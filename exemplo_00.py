b9486f7bcf0f2a05705f8bda8dbdd6fe9642abf2
Victor Contreras <contreras3991@gmail.com>
2025-01-07 15:01:51 -0300

    exemplo airflow custom solution

---
+++ exemplo_00.py
@@ -0,0 +1,28 @@
+## PIPELINE SEM RECORRENCIA
+
+from time import sleep
+
+def primeira_atividade():
+    print('minha primeira atividade')
+    sleep(2)
+
+
+def segunda_atividade():
+    print('minha segunda atividade')
+    sleep(2)
+
+
+def terceira_atividade():
+    print('minha terceira atividade')
+    sleep(2)    
+
+
+def pipeline():
+    primeira_atividade()
+    segunda_atividade()
+    terceira_atividade()
+    print('pipeline finalizada')
+
+
+if __name__ == '__main__':
+    pipeline()
